22c2d78d86186a0a10b318d07bbea9252bfb7c4e
lunglinhlaplanh <158856626+hungdev1602@users.noreply.github.com>
2025-08-17 01:28:33 +0300

    ghép code Frontend cho Trang chủ (bên client)

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,20 @@
 import express, { Request, Response, Express } from 'express';
+import path from "path"
 
 const app: Express = express()
 const port: number = 3000
 
+// view engine setup
+app.set("views", path.join(__dirname, "views")) // thư mục chứa file pug
+app.set("view engine", "pug") // thiết lập view engine là pug
+
+// thiết lập thư mục public, chứa file tĩnh (css, js, img,...)
+app.use(express.static(path.join(__dirname, "public")))
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!")
+  res.render("client/pages/home", {
+    pageTitle: "Home Page Zenis"
+  })
 })
 
 app.listen(port, () => {
