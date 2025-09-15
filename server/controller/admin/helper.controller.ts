04b6f85ad64a3a4eb1e561be2a5edb6a08060914
lunglinhlaplanh <158856626+hungdev1602@users.noreply.github.com>
2025-09-16 01:44:43 +0300

    thêm auto slug và có thể thay đổi slug

--- server/controller/admin/helper.controller.ts
+++ server/controller/admin/helper.controller.ts
@@ -5,28 +5,35 @@ import { generateRandomString } from "../../helpers/generate.helper"
 import mongoose from "mongoose"
 
 export const generateSlug = async (req: Request, res: Response) => {
-  const { string, modelName } = req.body
+  try {
+    const { string, modelName } = req.body
 
-  let slug = slugify(string, {
-    lower: true, // chuyển hết thành chữ in thường
-    strict: true // loại bỏ các ký tự đặc biệt
-  })
+    let slug = slugify(string, {
+      lower: true, // chuyển hết thành chữ in thường
+      strict: true // loại bỏ các ký tự đặc biệt
+    })
 
-  const Model = mongoose.model(modelName)
+    const Model = mongoose.model(modelName)
 
-  // Kiểm tra xem slug tồn tại hay chưa, nếu có thì thêm chuỗi random
-  const checkExistSlug = await Model.findOne({
-    slug: slug
-  })
+    // Kiểm tra xem slug tồn tại hay chưa, nếu có thì thêm chuỗi random
+    const checkExistSlug = await Model.findOne({
+      slug: slug
+    })
 
-  if(checkExistSlug){
-    const stringRandom = generateRandomString()
-    slug = slug + "-" + stringRandom
+    if(checkExistSlug){
+      const stringRandom = generateRandomString()
+      slug = slug + "-" + stringRandom
+    }
+
+    res.json({
+      code: "success",
+      message: "Tạo slug thành công",
+      slug: slug
+    })
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: "Model không hợp lệ!"
+    })
   }
-
-  res.json({
-    code: "success",
-    message: "Tạo slug thành công",
-    slug: slug
-  })
 }
