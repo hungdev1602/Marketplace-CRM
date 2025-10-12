d45dcc8d88ef2af596aae5183ab49a5e45ef7af2
lunglinhlaplanh <158856626+hungdev1602@users.noreply.github.com>
2025-10-13 00:23:48 +0300

    tính năng copy link file

--- server/public/admin/assets/js/main.js
+++ server/public/admin/assets/js/main.js
@@ -2,11 +2,7 @@
 const initialTinyMCE = () => {
   tinymce.init({
     selector: '[textarea-mce]',
-    plugins: [
-      'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
-      'checklist', 'mediaembed', 'casechange', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'advtemplate', 'uploadcare', 'mentions', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
-    ],
-    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography uploadcare | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat | image',
   });
 }
 
@@ -323,3 +319,17 @@ if(pagination) {
   })
 }
 // End Pagination
+
+// Button copy
+const listButtonCopy = document.querySelectorAll("[button-copy]")
+if(listButtonCopy.length > 0){
+  listButtonCopy.forEach(button => {
+    button.addEventListener("click", () => {
+      const contentCopy = button.getAttribute("content-copy")
+      navigator.clipboard.writeText(contentCopy)
+
+      notyf.success("Đã copy!")
+    })
+  })
+}
+// End Button copy
